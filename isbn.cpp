3c6c46367d4bf13173fe9701a576882e1396bc27
AngelGonzalezLeon <angelagonzalez@student.hartnell.edu>
2015-10-08 13:05:54 -0700

    Trying to make this work on github

--- isbn.cpp
+++ isbn.cpp
@@ -1,4 +1,4 @@
-// Joe Welch
+// Joe Welch, and Angel Gonzalez
 // ISBN Check for 10 digit values
 // Confirm correct values with: http://www.isbn-check.com/
 // 2 Oct 2015
@@ -21,6 +21,8 @@
 #include <string>
 #include <cctype>
 #include <cstdlib>
+#include <cassert>
+
 using namespace std;
 
 // must use these two functions
@@ -36,7 +38,15 @@ int main()
 {
 
     string strA = "0385353308";      // test case for ISBN10
-    string strB = "9780136091813";   // test case for ISBN13
+    string strB = "9780136091810";   // test case for ISBN13
+    
+    int strASum = ( sumISBN10Digits (strA) );
+    int strBSum = ( sumISBN10Digits (strB) );
+    
+    assert( isValidISBN10 (strASum) == true);
+    assert( isValidISBN13 (strBSum) == true);
+    
+    
     
       
     return 0;
@@ -75,7 +85,7 @@ int charToInt(char ch)
 {
     return (int(ch) - 48);
 
-}// charToInt(char ch)
+}//end charToInt(char ch)
 
 // ****************************
 bool isValidISBN10(int val)
@@ -98,9 +108,9 @@ int sumISBN13Digits(string val)
     int p;
     int num;
     
-    for(int ix = 0; ix < val.length() ; ix++)
+    for(int ix = 0; ix < val.length() - 1; ix++)
     {
-        p = charToInt(val.at(ix) ) ;
+        p = charToInt( val.at(ix) ) ;
         
         if (( ix % 2) == 0)
         {
@@ -115,7 +125,7 @@ int sumISBN13Digits(string val)
     }//end for()
     return sum;
 
-}// int sumISBN13Digits(string val)
+}//end int sumISBN13Digits(string val)
 
 // **********************************
 bool isValidISBN13(int val)
